ce313d57b93730d4f91cbfe195b2f1c3df1776e2
rohitGupta2022 <pta.rohit28@gmail.com>
2023-08-28 22:26:47 +0530

    Fix: Scroll Header effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from '@/components/About'
+import Experience from '@/components/Experience'
 import Intro from '@/components/Intro'
 import Projects from '@/components/Projects'
 import SectionDivider from '@/components/SectionDivider'
@@ -13,6 +14,7 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
+      <Experience />
     </main>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,7 @@ import clsx from 'clsx'
 import { useActiveSectionContext } from '@/context/active-section-context'
 
 function Header() {
-  const { activeSection, setActiveSection } = useActiveSectionContext()
+  const { activeSection, setActiveSection, setTimeOfLastClick } = useActiveSectionContext()
 
   return (
     <header className='z-[999] relative '>
@@ -38,7 +38,10 @@ function Header() {
                   }
                 )}
                   href={link.hash}
-                  onClick={() => setActiveSection(link.name)}
+                  onClick={() => {
+                    setActiveSection(link.name)
+                    setTimeOfLastClick(Date.now());
+                  }}
                 >
                   {
                     link.name
